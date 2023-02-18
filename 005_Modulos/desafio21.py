ce8cc775af1723a75aa2ecf969f89a0bf2844eb5
isahonorato <isabelladdotcom@gmail.com>
2023-02-18 00:12:11 -0300

    exercicios

--- 005_Modulos/desafio21.py
+++ 005_Modulos/desafio21.py
@@ -1,1 +1,9 @@
 #Faça um arquivo que abra e reproduza o áudio de um arquivo MP3. 
+
+import pygame 
+
+pygame.init()
+
+pygame.mixer.music.load('') #colocar o nome do arquivo mp3
+pygame.mixer.music.play()
+pygame.event.wait()
