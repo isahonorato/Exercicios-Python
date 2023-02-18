7fff9d6b9c114f5289e95f7d5f9a7c03638e7dd5
isahonorato <isabelladdotcom@gmail.com>
2023-02-17 23:10:33 -0300

    exercicios

--- 005_Modulos/desafio19.py
+++ 005_Modulos/desafio19.py
@@ -1,1 +1,13 @@
 #Um professor quer sortear um dos seus 4 alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido.
+import random
+
+aluno1 = str(input('Nome do primeiro aluno: '))
+aluno2 = str(input('Nome do segundo aluno: '))
+aluno3 = str(input('Nome do terceiro aluno: '))
+aluno4 = str(input('Nome do quarto aluno: '))
+
+lista = [aluno1, aluno2, aluno3, aluno4]
+escolhido = random.choice(lista)
+
+print( 'O aluno escolhido foi o {}'.format(escolhido))
+
