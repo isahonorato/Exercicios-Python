a9e3fdceeb6a1da354d0fecb5ce3f674f33ecafb
isahonorato <isabelladdotcom@gmail.com>
2023-02-19 16:48:42 -0300

    exercicios

--- 005_Modulos/desafio21.py
+++ 005_Modulos/desafio21.py
@@ -4,6 +4,6 @@ import pygame
 
 pygame.init()
 
-pygame.mixer.music.load('') #colocar o nome do arquivo mp3
+pygame.mixer.music.load('baby get on your knees') #colocar o nome do arquivo mp3
 pygame.mixer.music.play()
 pygame.event.wait()
