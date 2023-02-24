9a4e010bdcd9e702a4d476a29c10714f4eca39b3
isahonorato <isabelladdotcom@gmail.com>
2023-02-23 23:17:30 -0300

    Exercicios Python

--- 006_ManipulandoTexto/main.py
+++ 006_ManipulandoTexto/main.py
@@ -7,5 +7,18 @@ print(frase)
 
 #Análise
 frase.count('o') #contando quantas vezes há a letra "o"
-
 frase.find('ami') #contando em qual posição ele encontrou o "ami"
+
+'dia' in frase #Existe "dia" em frase? se sim, ele retornará TRUE
+
+#Transformação
+frase.replace('amigos,' 'Android') #Onde tiver amigos ele substituíra por android
+frase.upper() #Upper significa pra cima, ou seja, transforma em maiúsculas
+frase.lower() #Lower é para baixo, ou seja transforma em minúsculos
+frase.capitalize() #Joga todos os caracteres para minúsculos e somente o primeiro caractere em maiúsculo
+frase.title() #Analisa quantas palavras tem a frase e realiza o captalize palavra por palavra
+frase.strip() #Remove os espaços que houverem no começo da frase e no final
+frase.rstrip() #Remove somente os últimos espaços (direita) da string
+frase.lstrip() #Remove somente os espacços do começo (esquerda)
+
+#26:38
