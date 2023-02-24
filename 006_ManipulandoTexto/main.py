3d25ddaeeb49c877010a7089b4d3eb0f83bdbeb4
isahonorato <isabelladdotcom@gmail.com>
2023-02-23 23:04:36 -0300

    Exercicios Python

--- 006_ManipulandoTexto/main.py
+++ 006_ManipulandoTexto/main.py
@@ -1,6 +1,9 @@
 #Chamamos uma frase de 'cadeia de caracteres' ou string.
 
 #Fatiamento
-frase = 'Curso em vídeo Python!'
+frase = 'Bom dia amigos e amigas!'
 frase [5:4:2]
 print(frase)
+
+#Análise
+frase.count('o') #contando quantas vezes há a letra "o"
