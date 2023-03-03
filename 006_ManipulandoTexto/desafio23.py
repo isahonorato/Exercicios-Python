c2c817ede5368e437495d1e772b4a525fd3145be
isahonorato <isabelladdotcom@gmail.com>
2023-03-02 23:39:12 -0300

    Exercicios Python

--- 006_ManipulandoTexto/desafio23.py
+++ 006_ManipulandoTexto/desafio23.py
@@ -1,6 +1,10 @@
 #Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
-#Exemplo: Digite um número: 1834
-# unidade: 4
-# dezena: 3
-# centena: 8
-#milhar: 1
+
+numero = int(input('Digite um número: '))
+
+unidade = numero // 1
+
+print('Unidade: '.format())
+print('Dezena: '.format())
+print('Centena: '.format())
+print('Milhar: '.format())
