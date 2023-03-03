9d9b1046a10cbc0edbae0da703f1771527a9d344
isahonorato <isabelladdotcom@gmail.com>
2023-03-02 23:40:47 -0300

    Exercicios Python

--- 006_ManipulandoTexto/teste.py
+++ 006_ManipulandoTexto/teste.py
@@ -1,3 +1,28 @@
+raio = float(input())
+n = 3.14159
+area = n * raio ** 2
+print(f'A={area:.4f}')
+
+
+nota1 = float(input())
+nota2 = float(input())
+
+media = (((nota1 * 3.5) + (nota2 * 7.5)) / 11)
+
+print(f'MEDIA = {media:.5f}')
+
+
+
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+
+media = (((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) /10)
+
+print(f'MEDIA = {media:.1f}')
+
+
+
 A = int(input())
 B = int(input())
 C = int(input())
