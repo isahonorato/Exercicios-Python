26d45c1f4029224b3173801a5ba33e982bb18f68
isahonorato <isabelladdotcom@gmail.com>
2023-03-04 21:47:50 -0300

    Exercicios Python

---
+++ Beecrowd/bee1007.py
@@ -0,0 +1,18 @@
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+
+media = (((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) /10)
+
+print(f'MEDIA = {media:.1f}')
+
+
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+diferenca = (A * B - C * D)
+
+print(f'DIFERENCA = {diferenca}')
---
+++ Beecrowd/bee1014.py
@@ -0,0 +1,6 @@
+km = int(input())
+combustivel = float(input())
+
+consumo = km / combustivel
+
+print(f'{consumo:.3f} km/l')
