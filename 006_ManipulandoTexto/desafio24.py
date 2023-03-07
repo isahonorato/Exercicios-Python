fd91e763082dd994a3dc284d57aba254946652a6
isahonorato <isabelladdotcom@gmail.com>
2023-03-06 22:47:43 -0300

    Exercicios Python

--- 006_ManipulandoTexto/desafio24.py
+++ 006_ManipulandoTexto/desafio24.py
@@ -1,1 +1,5 @@
 #Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+
+cidade = str(input('Em que cidade você nasceu? ')).strip()
+
+print(cidade[:5].upper == 'SANTO')
