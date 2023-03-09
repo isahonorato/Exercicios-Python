39a31380b4412b2f3ce3a1fb42d7f7ce6cc8be96
isahonorato <isabelladdotcom@gmail.com>
2023-03-08 23:44:02 -0300

    Exercicios Python

--- 006_ManipulandoTexto/desafio25.py
+++ 006_ManipulandoTexto/desafio25.py
@@ -1,1 +1,5 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+nome = str(input('Qual é o seu nome completo?')).strip()
+
+print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
