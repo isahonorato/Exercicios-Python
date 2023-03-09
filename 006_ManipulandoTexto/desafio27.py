797838735b5c248f42009185c117bf6883ef41c4
isahonorato <isabelladdotcom@gmail.com>
2023-03-08 23:59:36 -0300

    Exercicios Python

--- 006_ManipulandoTexto/desafio27.py
+++ 006_ManipulandoTexto/desafio27.py
@@ -1,4 +1,10 @@
 #Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome.
 
+nome = str(input('Digite o seu nome completo: ')).strip()
+nomePessoa = nome.split() 
+
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[len(nome)]-1))
+
 
 
