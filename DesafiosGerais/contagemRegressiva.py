a3bce5604031225f922025e26019dba6b88676c9
isahonorato <isabelladdotcom@gmail.com>
2023-03-18 23:28:04 -0300

    Desafio contagem regressiva

---
+++ DesafiosGerais/contagemRegressiva.py
@@ -0,0 +1,11 @@
+#Faça um programa que realize uma contagem regressiva
+
+from time import sleep
+
+print("Contagem Regressiva")
+
+for cont in range (10, 0, -1):
+    print(cont)
+    sleep(1)
+
+print("CONTAGEM FINALIZADA!")
