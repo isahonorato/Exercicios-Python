9bcb9e8db07a74153672aeb7f46fa3491448bdb2
isahonorato <isabelladdotcom@gmail.com>
2023-03-23 23:19:44 -0300

    Exercícios Python

--- Beecrowd/teste.py
+++
@@ -1,8 +0,0 @@
-idade = int(input('Digite a sua idade: '))
-
-if (idade >= 18):
-     print('Maior de idade, pode dirigir!')
-elif (idade == 17):
-     print('')
-else:
-     print('Não pode dirigir!')
