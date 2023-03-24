789bfc4688e36b9dcec733b6251beb83c47dab3b
isahonorato <isabelladdotcom@gmail.com>
2023-03-23 21:25:37 -0300

    Exercícios Python

--- Beecrowd/bee1012.py
+++ Beecrowd/bee1012.py
@@ -1,9 +1,18 @@
-A = float(input())
-B = float(input())
-C = float(input())
+ABC = input()
+tratamento = ABC.split()
 
-print(f'TRIANGULO:')
-print(f'CIRCULO:')
-print(f'TRAPEZIO:')
-print(f'QUADRADO:')
-print(f'RETANGULO:')
+pi = 3.14159
+
+A = float(tratamento[0])
+B = float(tratamento[1])
+C = float(tratamento[2])
+
+
+print(f'TRIANGULO: {A * C / 2:.3f}')
+print(f'CIRCULO: {pi * C ** 2:.3f}')
+print(f'TRAPEZIO: {(A + B) * C / 2:.3f}')
+print(f'QUADRADO: {B ** 2:.3f}')
+print(f'RETANGULO: {A * B:.3f}')
+
+
+
